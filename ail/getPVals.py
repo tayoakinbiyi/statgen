65db6bbcc23d4bea10a9195f396392fd5bf361ef
takintayo akinbiyi <akinbiyi@midway2-login2.rcc.local>
2019-06-02 13:14:43 -0500

    made getPVals look like paper

--- ail/getPVals.py
+++ ail/getPVals.py
@@ -3,11 +3,12 @@ import numpy as np
 import os
 import pdb
 from scipy.stats import norm
-from collections import Counter
 from concurrent.futures import ProcessPoolExecutor, wait, ALL_COMPLETED, FIRST_COMPLETED
 from multiprocessing import cpu_count
 from sklearn.linear_model import LinearRegression
 from sklearn.multioutput import MultiOutputRegressor
+from collections import Counter
+
 from python.lmm import *
 from python.grm import *
 from python.genPCPval import *
@@ -15,28 +16,29 @@ from python.genPCPval import *
 #home='/phddata/akinbiyi/'
 home='/project/abney/'
 
-predExpr=False
+responseFile='hipRaw'
+predWrite=False
+genoWrite=False
+removePCs=True
 GRM=False
-genPC=True
-doLMM=True
+writeExpr=False
+doLMM=False
+numPCs=100
+
+#########################################################################################33
 
 files={
     'dataDir':home+'ail/data/',
     'scratchDir':home+'ail/scratch/',
     'gemma':home+'ail/gemma'
 }
-numPCs=100
-
 
 #load raw files
 dataDir=files['dataDir']
 scratchDir=files['scratchDir']
 
-# snps.txt
 print('load raw')
-snps=pd.read_csv(dataDir+'ail.genos.dosage.gwasSNPs.txt',sep='\t',header=None,index_col=None)
-preds=pd.read_csv(dataDir+'ail.phenos.final.txt',sep='\t',header=0,index_col=0)
-expr=pd.read_csv(dataDir+'hipNormCounts.expr.txt',sep='\t',index_col=0,header=0)
+expr=pd.read_csv(dataDir+responseFile+'.txt',sep='\t',index_col=0,header=0)
 
 print('load traitInfo')
 traitInfo=pd.read_csv(dataDir+'traitInfo.csv',header=0,index_col=None)
@@ -51,82 +53,107 @@ traitChr=pd.DataFrame({'trait':expr.columns}).merge(traitInfo,on='trait')
 expr=expr[traitChr.trait]
 
 # get list of person IDs
-allIds=preds.index.values.flatten().tolist()
 exprIds=expr.index.values.flatten().tolist()
 
-# format SNP file
-snps.columns=['chr','snpId','minor','major']+allIds
-snpChr=snps['chr'].values.flatten()
-snpId=snps['snpId'].values.flatten()
-snps.drop(columns='chr',inplace=True)
-snps=snps[['snpId','minor','major']+exprIds]
+os.chdir(scratchDir)
 
-if predExpr:
+if predWrite:
+    preds=pd.read_csv(dataDir+'ail.phenos.final.txt',sep='\t',header=0,index_col=0)
+
+    allIds=preds.index.values.flatten().tolist()
+    np.savetxt('allId.csv',allIds,delimiter=',')
+
     # recover sex and batch data
     print('prep covs')
     preds =preds.loc[exprIds,['sex','batch']]
     preds.insert(0,'intercept',1)
+    
     preds=preds.values
+    np.savetxt('preds.txt',preds,delimiter='\t')
+    
+else:
+    allIds=np.loadtxt('allId.csv',delimiter=',')
 
+if genoWrite:
+    snps=pd.read_csv(dataDir+'ail.genos.dosage.gwasSNPs.txt',sep='\t',header=None,index_col=None)
+    
+    # format SNP file
+    snps.columns=['chr','snpId','minor','major']+allIds
+    snpChr=snps['chr'].values.flatten()
+    snpId=snps['snpId'].values.flatten()
+    snps.drop(columns='chr',inplace=True)
+    snps=snps[['snpId','minor','major']+exprIds]
+
+    print('write one snp geno files')
+    for snp in set(snpChr):
+        snps[snpChr==snp].to_csv('geno-'+snp+'.txt',sep='\t',index=False,header=False)
+    
+    pd.DataFrame({'id':snpId,'chr':snpChr}).to_csv(dataDir+'snpId.csv',index=False)
+else:
+    snpData=pd.read_csv(dataDir+'snpId.csv')
+    snpId=snpData['id']
+    snpChr=snpData['chr']
+    
+if GRM:
+    np.savetxt('dummy.txt',np.ones([len(expr),1]),delimiter='\t')
+
+    print('generate grm and genome files')
+
+    futures=[]
+    with ProcessPoolExecutor() as executor: 
+        for snp in set(snpChr):
+            futures.append(executor.submit(grm,snp,snpChr,snps,files))
+
+    wait(futures,return_when=ALL_COMPLETED)
+    
+if removePCs:
     # quantile normalize expr data
     print('quant normalize')
     ranks=expr.rank(axis=0,method='average')/(len(expr)+1)
     expr=ranks.apply(norm.ppf,axis=0).values
 
-    # remove sex and batch
-    print('remove batch, sex')
-    reg=MultiOutputRegressor(LinearRegression(),n_jobs=-1).fit(preds,expr)
-    expr=(expr-reg.predict(preds))
-
-    print('write traits to file')
-    for trait in set(traitChr.chromosome):
-        np.savetxt(scratchDir+'pheno-'+trait+'.txt',expr[:,traitChr.chromosome==trait],delimiter='\t')
-        
     # generate PCs
     print('generate PCs')
     U,D,Vt=np.linalg.svd(expr)
-    PCs=np.concatenate([np.ones([len(expr),1]),U[:,0:numPCs]],axis=1)
+    PCs=U[:,0:numPCs]
 
     # write covariate data to file
     print('write PCs to file')
-    np.savetxt(scratchDir+'PCAll.txt',PCs,delimiter='\t')
+    np.savetxt('PCAll.txt',PCs,delimiter='\t')
 
-if GRM:
-    np.savetxt(scratchDir+'dummy.txt',np.ones([len(expr),1]),delimiter='\t')
-
-    print('generate grm and genome files')
-    os.chdir(scratchDir)
-
-    futures=[]
-    with ProcessPoolExecutor() as executor: 
-        for ch in set(snpChr):
-            futures.append(executor.submit(grm,ch,snpChr,snps,files))
-
-    wait(futures,return_when=ALL_COMPLETED)
-    
-if genPC:
     print('Find PCs')
-    os.chdir(scratchDir)
-    
     futures=[]
     with ProcessPoolExecutor() as executor: 
         for snp in set(snpChr):
-            futures.append(executor.submit(genPCPval,snp,numPCs,files))
-
+            futures.append(executor.submit(genPCPval,snp,snpId[snpChr==snp],numPCs,files))
+    
     wait(futures,return_when=ALL_COMPLETED)
     print('finished genPCPval')
     
     PCPval=[]
-    count=0
     for snp in set(snpChr):
-        PCPval[count]=np.loadtxt('pvals-PC-'+snp+'.txt',delimiter='\t')
+        PCPval+=[np.loadtxt('pvals-PC-'+snp+'.txt',delimiter='\t')]
         
     PCPval=np.concatenate(PCPval,axis=0)
-        
+    pdb.set_trace()
     whichPCs=np.arange(numPCs)[np.min(PCPval,axis=0)>9.01e-6]
-    print('PCs ',whichPCs)
+    print('PCs ',whichPCs,' # ',len(whicPCs))
+        
+    # remove PCs
+    print('PCs')
+    reg=MultiOutputRegressor(LinearRegression(),n_jobs=-1).fit(PCs[:,whichPCs],expr)
+    expr=(expr-reg.predict(PCs[:,whichPCs]))    
+
+    print('quant normalize')
+    ranks=expr.rank(axis=0,method='average')/(len(expr)+1)
+    expr=ranks.apply(norm.ppf,axis=0).values
     
-    PCs=np.savetxt(scratchDir+'PC.txt',np.loadtxt('PCAll.txt',delimiter='\t')[:,whichPCs],delimiter='\t')
+    var=np.diag(np.cov(expr,rowvar=True))
+
+if writeExpr:
+    print('write traits to file')
+    for trait in set(traitChr.chromosome):
+        np.savetxt('pheno-'+trait+'.txt',expr[:,traitChr.chromosome==trait],delimiter='\t')        
 
 # loop through traits and chromosomes
 if doLMM:
--- ail/python/genPCPval.py
+++ ail/python/genPCPval.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import subprocess
 import pdb
 import os
@@ -7,17 +8,21 @@ import sys
 def genPCPval(snp,snpId,numPCs,files):
     gemma=files['gemma']
     
-    if os.path.isfile('pvals-final-'+snp+'-'+trait+'.txt'):
-        pass
-        #return()
-
+    if os.path.isfile('pvals-PC-'+snp+'.txt'):
+        return()
+    
     PCs=np.empty([len(snpId),numPCs])
 
     for k in range(numPCs):
-        subprocess.run([gemma,'-g','geno-'+str(snp)+'.csv','-p','PCAll.txt','-lmm','2','-o',
-            'pvals-'+snp,'-k','grm-'+snp+'.sXX.csv','-maf','0.05','-r2','0.99','-n',str(k+1)])
+        subprocess.run([gemma,'-g','geno-'+str(snp)+'.txt','-p','PCAll.txt','-lmm','2','-o','pvals-'+snp,'-k','grm-'+snp+'.txt','-maf',
+            '0.05','-r2','0.99','-n',str(k+2)])
         
-        PCs[:,k]=pd.read_csv('output/pvals-'+snp+'.assoc.txt',sep='\t')['p_score'].values.flatten()
-      
+        os.remove('output/pvals-'+snp+'.log.txt')
+        
+        PCs[:,k]=pd.read_csv('output/pvals-'+snp+'.assoc.txt',sep='\t')['p_lrt'].values.flatten()
+    
+        os.remove('output/pvals-'+snp+'.assoc.txt')
+    
+    sys.stderr.write(snp) 
     np.savetxt('pvals-PC-'+snp+'.txt',PCs,delimiter='\t')
     
