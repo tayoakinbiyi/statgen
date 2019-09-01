cd42e2384faa5e7bcd89948079b439c2ac5fb1e4
Ubuntu <ubuntu@ip-172-31-15-52.us-west-2.compute.internal>
2019-09-01 04:25:39 +0000

    ..

--- ail/dataPrepPython/score.py
+++ ail/dataPrepPython/score.py
@@ -22,7 +22,7 @@ def score(parms):
     pre=('z-' if parms['wald'] else 'p-')
     pdb.set_trace()
     for file in os.listdir(local+name+'score'):
-        data=np.readtxt(local+name+file,delimiter=',')
+        data=np.loadtxt(local+name+file,delimiter=',')
         DBWrite(data,name+'score/'+file[:-4],parms)
     pdb.set_trace()
      
