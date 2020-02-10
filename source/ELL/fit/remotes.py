b4eb1b25eb9b73fa1a21d81c75f8708a0ad1dc4d
akinbiyi <akinbiyi@newgrange.uchicago.edu>
2020-02-10 13:28:03 -0600

    Update 8886 items. Added 8886 items. Removed 3672 items.

---
+++ source/ELL/fit/remotes.py
@@ -0,0 +1,52 @@
+import numpy as np
+import ray
+from scipy.stats import norm
+import pdb
+import warnings
+import os
+import time
+
+#from ELL.fit.nonClassMethods import *
+
+@ray.remote
+def f(N,lam,minK,maxK,nCr,offDiagMeans):
+    assert 0<lam<1
+    
+    z=-norm.ppf(lam/2)
+
+    He1 = z**2
+    He3 = (z**3-3*z)**2
+    He5 = (z**5-10*z**3+15*z)**2
+    He7 = (z**7-21*z**5+105*z**3-105*z)**2
+    He9 = (z**9-36*z**7+378*z**5-1260*z**3+945*z)**2
+    
+    odds = ( He1*offDiagMeans[1]/2 + He3*offDiagMeans[3]/24 + He5*offDiagMeans[5]/720 + He7*offDiagMeans[7]/40320 + 
+            He9*offDiagMeans[9]/3628800 )
+    
+    x=4*norm.pdf(z)**2*odds    
+    gamma=(x/(lam*(1-lam)-x))
+    
+    baseOne=np.append([0],np.cumsum(np.log(lam+gamma*np.arange(0,maxK))))
+    baseTwo=np.cumsum(np.log(1-lam+gamma*np.arange(N)))[-(maxK+1):][::-1]
+    baseThree=np.sum(np.log(1+gamma*np.arange(N)))
+    baseCr=nCr[0:(int(maxK)+1)]
+
+    Pr=np.exp(baseCr+baseOne+baseTwo-baseThree)
+    
+    return(1-np.cumsum(Pr)[minK:maxK+1])
+    
+@ray.remote
+def lamEllByK(core,binRange,N,rightEdgePerBin,minKPerBin,maxKPerBin,nCr,lamEllByK,ellGrid,offDiagMeans):
+    print('lamEllByK.remote ({}): lamEllByK {}'.format(os.getpid(), id(lamEllByK)))
+    rightEdgePerBin=rightEdgePerBin[binRange]
+    minKPerBin=minKPerBin[binRange]
+    maxKPerBin=maxKPerBin[binRange]
+    
+    for Bin in range(len(binRange)):  
+        kList=np.arange(minKPerBin[Bin],maxKPerBin[Bin]+1).astype(int)
+        fval=ray.get(f.remote(N,rightEdgePerBin[Bin],minKPerBin[Bin],maxKPerBin[Bin],nCr,offDiagMeans))  
+        gridLoc=np.searchsorted(ellGrid,fval[::-1])[::-1]
+        upd=(gridLoc<len(ellGrid))        
+        lamEllByK[gridLoc[upd],kList[upd]]=np.maximum(rightEdgePerBin[Bin],lamEllByK[gridLoc[upd],kList[upd]])
+        
+    return()
--- source/simPython/makeSimSnps.py
+++ source/simPython/makeSimSnps.py
@@ -2,22 +2,25 @@ import numpy as np
 import pandas as pd
 import random
 import subprocess
+import pdb
 
 def makeSimSnps(parms): 
     snpSize=parms['snpSize']
     maxSnpGen=parms['maxSnpGen']
     local=parms['local']
     numSubjects=parms['numSubjects']
-    
+        
     numSnps=np.sum(snpSize)
     subprocess.call(['cp',local+'ext/sampleIds.txt','geneDrop/sampleIds.txt'])
     pd.DataFrame({'parms':[local+'ext/ail_revised.ped.txt','geneDrop/sampleIds.txt','geneDrop/map.txt',0,0]}).to_csv(
         'geneDrop/parms.txt',index=False,header=None)
+    
+    tab=str.maketrans('1234','AAGG')
 
     cols=[]
     t_numSnps=0
     while t_numSnps<numSnps:
-        newAdd=min(maxSnpGen,t_numSnps-numSnps)
+        newAdd=min(maxSnpGen,numSnps-t_numSnps)
         pd.DataFrame({'# name':np.arange(1,newAdd+1),'length(cM)':1,'spacing(cM)':2,'MAF':.5}).to_csv(
             'geneDrop/map.txt',sep='\t',index=False)
 
@@ -26,17 +29,16 @@ def makeSimSnps(parms):
         while t_numSubjects<numSubjects:
             cmd=[local+'ext/gdrop','-p','geneDrop/parms.txt','-s',str(random.randint(1,1e6)),'-o','geneDrop/geneDrop']
             subprocess.run(cmd)
+            rows+=[np.loadtxt('geneDrop/geneDrop.geno_true',delimiter='\t',dtype=str)[:,4:4+(numSubjects-t_numSubjects)].T]
 
-            row+=[np.loadtxt('geneDrop/geneDrop.geno_true',delimiter='\t')[(numSnps-t_numSnps),4:4+(numSubjects-t_numSubjects)].T]
-
-            t_numSubjects+=row[-1].shape[0]
+            t_numSubjects+=rows[-1].shape[0]
             
         rows=np.concatenate(rows,axis=0)
         
-        maf=np.mean(np.apply_over_axes(lambda string: np.sum(np.array(string.split(' ')).isin(['3','4'])),rows,[0,1]),axis=0)/2
+        maf=np.mean([[sum([int(z)>2 for z in y.split(' ')]) for y in x] for x in rows],axis=0)/2            
         maf=np.minimum(maf,1-maf)
         rows=rows[:,maf>.1]
-        rows=np.char.translate(rows,{'1':'A','2':'A','3':'G','4':'G'})
+        rows=np.char.translate(rows,tab)
         
         cols+=[rows]
         t_numSnps+=cols[-1].shape[1]
