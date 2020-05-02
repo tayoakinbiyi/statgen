599ba088fc08154c9f58e0e51dc19ec7122eebeb
tayakinbiyi <akinbiyi2000@gmail.com>
2020-05-02 13:35:19 -0400

    1000 mice, 100 snps, 30 traits, subsample 0.1, Y is empirical-0

--- test5.py
+++ test5.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use('agg')
 import numpy as np
 import pdb
+
 import sys
 import os
 
@@ -9,11 +10,12 @@ sys.path=[os.getcwd()+'/source']+sys.path
 from dill.source import getsource
 from dataPrepPython.makeSim import *
 from dataPrepPython.runLimix import *
-from dataPrepPython.runH1 import *
 from multiprocessing import cpu_count
 from plotPython.plotPower import *
 from plotPython.plotZ import *
 from utility import *
+from plotPython.myHist import *
+from limix.model.lmm import LMM
 
 from statsPython.gbj import *
 
@@ -26,84 +28,130 @@ from rpy2.robjects.packages import importr
 def myMain(parms):
     gbjR=importr('GBJ')
                 
+    numSnps=parms['numDataSnps']
     numTraits=parms['numTraits']
-    
-    #######################################################################################################
-    #######################################################################################################
-    #######################################################################################################
-    
-    Y,QS,M,snps,Lgrm=makeSim(parms)
-    vY=np.corrcoef(Y,rowvar=False)
-    plotCorr(vY,'vY')
-    myHist(vY[np.triu_indices(numTraits,1)],'vY-hist')
-    
-    #######################################################################################################
-    #######################################################################################################
-    #######################################################################################################
-    
-    wald,eta=runLimix(Y,QS,M,snps)
-    plotZ(wald,prefix='wald-')
-    
-    #######################################################################################################
-    #######################################################################################################
-    #######################################################################################################
-        
-    vZ=np.corrcoef(wald,rowvar=False)
-    L=makePSD(vZ,'LvZ')
-    plotCorr(vZ,'vZ')
-    offDiag=vZ[np.triu_indices(numTraits,1)]    
-    myHist(offDiag,'vZ-hist')
 
     #######################################################################################################
     #######################################################################################################
     #######################################################################################################
     
-    zDep=np.matmul(norm.rvs(size=[int(1e5),ctrl['numTraits']]),L.T)
-    zIndep=norm.rvs(size=[int(1e5),ctrl['numTraits']])
+    Y,QS,M,snps,grm=makeSim(parms)    
+    plotCorr(grm,'grm')
+    np.savetxt('snps',snps,delimiter='\t')
+    np.savetxt('Y',Y,delimiter='\t')
     
     #######################################################################################################
     #######################################################################################################
     #######################################################################################################
+    
+    wald,eta=runLimix(Y,QS,M,snps,0.9999)
+    np.savetxt('wald',wald,delimiter='\t')
+    myHist(eta,'eta'+str(numTraits))
+    myQQ(eta,np.mean(wald**2,axis=0),'eta','wald**2')
+    
+    wald=np.loadtxt('wald',delimiter='\t')
+    
+    plotZ(wald,'wald')
         
-    stat=ELL.ell.ell(int(.3*numTraits),numTraits,qList=[0.1,0.05,0.01,0.001])
-    stat.preCompute(1e3,offDiag=offDiag)
-    gnull=stat.gnullScore(wald)
-    pre=stat.preScore(wald)
-    stat.plot(stat.monteCarlo(stat.preScore(zDep),pre),'diagnostics/ellDepMC-Y:{}'.format(parms['yParm']))
-    #stat.plot(gbj(gbjR.GBJ,wald,offDiag=offDiag),'diagnostics/gbjDep-Y:{}'.format(parms['yParm']))
-    stat.plot(stat.markov(pre),'diagnostics/ellDepMarkov-Y:{}'.format(parms['yParm']))
-    stat.plot(stat.monteCarlo(stat.gnullScore(zIndep),gnull),'diagnostics/ellIndepMC-Y:{}'.format(parms['yParm']))
-    #stat.plot(gbj(gbjR.GBJ,wald),'diagnostics/gbjIndep-Y:{}'.format(parms['yParm']))
+    #######################################################################################################
+    #######################################################################################################
+    #######################################################################################################
+
+    vZ=np.corrcoef(wald,rowvar=False)
+    plotCorr(vZ,'vZorig')
+    offDiagOrig=vZ[np.triu_indices(vZ.shape[1],1)]    
+    myHist(offDiagOrig,'offDiagOrig')
+    
+    U,lam,Vt=np.linalg.svd(vZ)
+    gamma=numTraits/parms['numSubjects']
+    phase=(1+np.sqrt(gamma))**2
+
+    lrg=np.where(lam>phase)[0]
+    sml=np.where(lam<=phase)[0]
+    
+    l=(lam[lrg]+1-gamma+np.sqrt((lam[lrg]+1-gamma)**2-4*lam[lrg]))/2
+    c=np.sqrt((1-gamma/(l-1)**2)/(1+gamma/(l-1)))
+    s=np.sqrt(1-c**2)
+
+    if parms['V(Z)']=='operator':
+        lam[lrg]=l
+        lam[sml]=1
+    if parms['V(Z)']=='frobenius':
+        lam[lrg]=l*c**2+s**2
+        lam[sml]=1
+    if parms['V(Z)']=='stein':
+        lam[lrg]=l/(c**2+l*s**2)
+        lam[sml]=1
+    if parms['V(Z)']=='frechet':
+        lam[lrg]=(s**2+np.sqrt(l)*c**2)**2
+        lam[sml]=1
+    if parms['V(Z)']=='simple':
+        lam[:]=lam[:]
+    if parms['V(Z)']=='eye': 
+        lam[:]=1
+
+    vZ=U@np.diag(lam)@U.T
+    makeCov=np.diag(1/np.sqrt(np.diag(vZ)))
+    vZ=makeCov@vZ@makeCov
+    plotCorr(vZ,'vZorig')
+
+    offDiag=vZ[np.triu_indices(vZ.shape[1],1)]    
+
+    myHist(offDiag,'offDiagNew')
+    myQQ(offDiagOrig,offDiag,'orig','new')
+    LZ=makeL(vZ)    
+
+    #######################################################################################################
+    #######################################################################################################
+    #######################################################################################################
+
+    zRef=np.matmul(norm.rvs(size=[int(1e6),parms['numTraits']]),LZ.T)
+
+    #######################################################################################################
+    #######################################################################################################
+    #######################################################################################################
+        
+    stat=ELL.ell.ell(int(parms['d']*parms['numTraits']),parms['numTraits'],offDiag)
+    #stat.plot(stat.exact(wald),'diagnostics/ellExact-Y:{}'.format(parms['yParm']))
+    
     stat.preCompute(1e3)
-    stat.plot(stat.markov(gnull),'diagnostics/ellIndepMarkov-Y:{}'.format(parms['yParm']))
+    pre=stat.score(wald)
+    ref=stat.score(zRef)
+    stat.plot(stat.monteCarlo(ref,pre),'diagnostics/ellMC-Y')
+    stat.plot(stat.markov(pre),'diagnostics/ellMarkov-Y')        
+    stat.plot(gbj(gbjR.GBJ,wald,offDiag=offDiag),'diagnostics/gbj')
+    stat.plot(gbj(gbjR.GHC,wald,offDiag=offDiag),'diagnostics/ghc')
     
 
 ops={
     'response':'hipRaw',
-    'snpSeed':0,
-    'ySeed':0,
+    'seed':5754,
     'maxSnpGen':5000,
-    'numGrmSnps':10000,
-    'eta':0.3,
+    'numGrmSnps':100,
+    'eta':0.3
 }
 
 ctrl={
-    'numSubjects':1200,
-    'numDataSnps':10000,
-    'numTraits':1000,
-    'pedigreeMult':0.5,
-    'yParm':'indep'
+    'numSubjects':1000,
+    'numDataSnps':100,
+    'numTraits':30,
+    'pedigreeMult':.1,
+    'snpParm':'geneDrop',
+    'd':0.2,
+    'traitCorrSource':'empirical',
+    'traitCorrRho':0,
+    'V(Z)':'simple'
 }
 
 #######################################################################################################
 
 parms={**ctrl,**ops}
-
 setupFolders()
-diagnostics()    
+
+diagnostics(parms['seed'])
 log(parms)
-    
+
 myMain(parms)
 
-git('{} mice, {} snps, {} traits, subsample {}, Y is {}'.format(ctrl['numSubjects'],ctrl['numDataSnps'],
-    ctrl['numTraits'],ctrl['pedigreeMult'],ctrl['yParm']))
+git('{} mice, {} snps, {} traits, subsample {}, Y is {}-{}'.format(parms['numSubjects'],parms['numDataSnps'],
+    parms['numTraits'],parms['pedigreeMult'],parms['traitCorrSource'],parms['traitCorrRho']))
