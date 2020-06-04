032f3ae8879956e8f2b8587edb3d85d9a8e719bf
tayoakinbiyi <akinbiyi2000@gmail.com>
2020-06-03 19:21:27 -0500

    1200 mice, 10000 snps, 1200 traits, subsample 0.1, rho 1

--- test1.py
+++ test1.py
@@ -230,7 +230,7 @@ ops={
 ctrl={
     'numSubjects':1200,
     'numH0Snps':10000,
-    'numH1Snps':200,
+    'numH1Snps':300,
     'numTraits':1200,
     'pedigreeMult':.1,
     'snpParm':'geneDrop',
@@ -244,10 +244,10 @@ ctrl={
     'maxIter':1e2,
     'numHermites':150,
     'numCores':cpu_count(),
-    'fit':['loadH0','loadH1','plotPower','loadPsi','loadY','fitPower','loadRef'],
+    'fit':['loadH0','fitH1','plotPower','loadPsi','loadY','fitPower','loadRef'],
     'n_assoc':[10],#,30,50,70,80,100,150],
     'mcMethodNames':['ELL'],#,'cpma','score','storey','minP'],
-    'markovMethodNames':['markov']
+    'markovMethodNames':[]#'markov']
 }
 
 #######################################################################################################
