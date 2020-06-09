39814ed62f6ff898d72ad749da87a7e45705d9ff
tayoakinbiyi <akinbiyi2000@gmail.com>
2020-06-08 19:18:03 -0500

    1200 mice, 10000 snps, 1200 traits, subsample 0.1, rho 1

--- test1.py
+++ test1.py
@@ -240,7 +240,7 @@ ctrl={
     'maxRefReps':int(1e5),
     'minEta':1e-12,
     'numLam':1e3,
-    'beta':2.9,#np.sqrt(10.2-1.58*np.log(n_assoc)),
+    'beta':2.85,#np.sqrt(10.2-1.58*np.log(n_assoc)),
     'eps':1e-13,
     'maxIter':1e2,
     'numHermites':150,
