75dbf49730c263760abfc1ff7b788037ad2dc318
tayoakinbiyi <akinbiyi2000@gmail.com>
2020-06-20 02:07:42 -0500

    power 0

--- test1.py
+++ test1.py
@@ -264,8 +264,8 @@ setupFolders()
 log(parms)
 
 
-h1Vals=np.array([(1,3.11)],dtype=[('n_assoc','int'),('effectSize','float64')])
-#h1Vals=np.array([(1,3.14),(2,3.125),(4,2.89),(10,2.568),(50,2),(150,1.53),(500,1.3)],dtype=[('n_assoc','int'),('effectSize','float64')]),(800,1.15)
+h1Vals=np.array([(2,3.125)],dtype=[('n_assoc','int'),('effectSize','float64')])
+#h1Vals=np.array([(1,3.1115),(2,3.125),(4,2.89),(10,2.568),(50,2),(150,1.53),(500,1.3)],dtype=[('n_assoc','int'),('effectSize','float64')]),(800,1.15)
 
 power=[]
 for run in range(1):
