c20a61bf2acb74c9a65e5dac07c4fdb87a8c7e16
tayoakinbiyi <akinbiyi2000@gmail.com>
2020-06-22 14:53:28 -0500

    power 0

--- test1.py
+++ test1.py
@@ -261,12 +261,12 @@ ctrl={
 parms={**ctrl,**ops}
 setupFolders()
 
-h1Vals=np.array([(1,2.4)],dtype=[('n_assoc','int'),('effectSize','float64')])
+h1Vals=np.array([(1,3)],dtype=[('n_assoc','int'),('effectSize','float64')])
 #h1Vals=np.array([(1,3.1115),(2,3.02),(4,2.915),(10,2.516),(50,1.93),(150,1.44),(500,1.27)],dtype=[('n_assoc','int'),('effectSize','float64')])#,(800,1.15)
 power=[]
 for run in range(1):
     #_=myMain({**parms,'n_assoc':None,'effectSize':None,'numDataSnps':10000,'fit':['runLimix','fitY','fitVz','fitPsi','fitRef']}) # create wald for H1
-    _=myMain({**parms,'n_assoc':None,'effectSize':None,'numDataSnps':300,'fit':['runLimix']}) # create wald for H1
+    #_=myMain({**parms,'n_assoc':None,'effectSize':None,'numDataSnps':300,'fit':['runLimix']}) # create wald for H1
     createDiagnostics(parms['seed'])
 
     log(parms)
